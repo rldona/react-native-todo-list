8408d7d61bc7f957f2f9eeb33ffedab27c59c06f
Raúl López Doña <rldona@gmail.com>
2021-03-15 20:29:47 +0100

    screens fixed

--- App.js
+++ App.js
@@ -1,47 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from './HomeScreen';
-import DetailScreen from './DetailScreen';
+import HomeScreen from './Screens/HomeScreen';
+import DetailScreen from './Screens/DetailScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home" 
-          component={ HomeScreen }
-          options={{
-            headerTitle: '_Home_',
-            headerStyle: {
-              backgroundColor: styles.header.backgroundColor,
-            },
-            headerTintColor: styles.header.tintColor,
-            headerTitleStyle: {
-              fontWeight: styles.header.fontWeight,
-            },
-          }}
-        />
-        <Stack.Screen 
-          name="Details" 
-          component={ DetailScreen }
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <StatusBar 
+        hidden={false} 
+      />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen 
+            name="Home" 
+            component={ HomeScreen }
+            options={{
+              headerTitle: 'Home',
+              headerStyle: {
+                backgroundColor: styles.header.backgroundColor,
+              },
+              headerTintColor: styles.header.tintColor,
+              headerTitleStyle: {
+                fontWeight: styles.header.fontWeight,
+              },
+            }}
+          />
+          <Stack.Screen 
+            name="Details" 
+            component={ DetailScreen }
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
   header: {
     backgroundColor: '#f4511e',
